c11f4b8eb72e91c125135f7efae8a8c32c8cabc3
Y.V <straysheep753@gmail.com>
2025-08-20 19:58:32 -0600

    5. Menu creation

--- Calculadora.py
+++ Calculadora.py
@@ -7,10 +7,28 @@ Implicados:
 dawg
 """
 
+
 key = True
 while key:
     try:
-        pass
+        operations = input("----------MENU----------\n\n"
+                           "1. Crear Meta y Plan de Ahorro\n"
+                           "2. Hacer un Depósito\n"
+                           "3. Ver Resumen de Metas y Planes\n"
+                           "4. Salir\n\n"
+                           "Opción: ")
+
+        match operations:
+            case "1":
+                pass
+            case "2":
+                pass
+            case "3":
+                pass
+            case "4":
+                print("Gracias por usar el programa")
+                key = False
+
     except Exception as e:
         print(f"Ha ocurrido un error: {e}")
 
